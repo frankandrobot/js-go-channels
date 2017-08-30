c224344e64295a43d14f594cf54b2c614908a16b
Uriel Avalos <uriel@rstudio.com>
2017-08-30 00:23:20 -0500

    first pass

--- src/index.js
+++ src/index.js
@@ -1,19 +1,93 @@
 import "babel-polyfill"
 
-function *foo() {
-  yield 1;
-  yield 2;
+function *chanId() {
+  let chanId = 0
+  for(;;) {
+    yield ++chanId
+  }
 }
 
-const gen = foo();
+const chanIds = chanId()
+const channels = {}
+const goRoutines = []
 
-console.log(gen.next().value)
-console.log(gen.next().value)
+class Channel {
+  constructor() {
+    this.id = chanIds.next().value
+  }
 
-const iterable = {
-  [Symbol.iterator]: foo,
+  take() {
+    return new TakeRequest(this)
+  }
+
+  put(msg) {
+    return new PutRequest(this, msg)
+  }
 }
 
-for(let i of iterable) {
-  console.log(i)
+class TakeRequest {
+  constructor(chan) {
+    this.chan = chan
+  }
 }
+
+class PutRequest {
+  constructor(chan, msg) {
+    this.chan = chan
+    this.msg = msg
+  }
+}
+
+function dispatcher(channels) {
+  goRoutines.forEach((goRoutine, i) => {
+    const {generator} = goRoutine
+    let {request} = goRoutine
+    // if no request, then get one
+    if (!request) {
+      request = generator.next().value
+      Object.assign(goRoutine, {request})
+    }
+    // otherwise we "block" i.e., don't get anymore requests until we
+    // satisfy the current request
+    const {chan: {id} = {}, msg} = request || {}
+    if (request instanceof TakeRequest) {
+      // do we have put data?
+      const channelData = channels[id]
+      if (channelData) {
+        // give the data to the generator and get the next request
+        request = generator.next(channelData).value
+        Object.assign(goRoutine, {request})
+      }
+    } else if (request instanceof PutRequest) {
+      // we gots data to give
+      channels[id] = msg
+      request = generator.next().value
+      if (request) {
+        Object.assign(goRoutine, {request})
+      }
+    }
+  })
+  if (goRoutines.length) {
+    setTimeout(() => dispatcher(channels), 0)
+  }
+}
+
+setTimeout(() => dispatcher(channels), 0)
+
+function go(iterator) {
+  goRoutines.push({
+    generator: iterator(),
+  })
+}
+
+// test
+const ch = new Channel()
+
+go(function*() {
+  yield ch.put('hello')
+})
+
+go(function*() {
+  const msg = yield ch.take()
+  console.log(msg)
+})
