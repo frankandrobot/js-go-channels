1d0a3e40e26c6cfd012136f6472cccf361de78bd
Uriel Avalos <uriel@rstudio.com>
2017-08-31 00:54:14 -0500

    [test] hooked up tests

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,18 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs';
+import fs from 'fs'
+
+// .babelrc doesn't exist because it overrides everything and tests
+// need a different babel config
+const babelrc = JSON.parse(fs.readFileSync('./babelrc.json'))
+const babelConfig = Object.assign(
+  {
+    exclude: 'node_modules/**',
+    runtimeHelpers: true,
+  },
+  babelrc
+)
 
 export default {
   input: 'src/index.js',
@@ -18,10 +30,8 @@ export default {
       jsnext: true,
       main: true,
       browser: true,
-      //modulesOnly: true,
-    }),
-    babel({
-      exclude: 'node_modules/**',
-    }),
+      modulesOnly: true,
+     }),
+    babel(babelConfig),
   ],
 }
