46a1217e76ee696fbeaf312c23c27af3d381279b
Uriel Avalos <uriel@rstudio.com>
2017-09-27 23:04:33 -0500

    [meta] fixed babelrc, creating proper libs for distribution

--- packages/js-go-channels/babeltest.config.js
+++ packages/js-go-channels/babeltest.config.js
@@ -2,10 +2,9 @@ const fs = require('fs')
 const path = require('path')
 const babelrcs = JSON.parse(fs.readFileSync(path.join(__dirname, '.babelrc')))
 // use the legacy-node environment
-const babelrc = babelrcs.env['legacy-node']
-// but kill the plugins
-babelrc.plugins = []
-
+const babelrc = babelrcs.env['legacy']
 // register babel on all test files
 require('babel-register')(babelrc)
-require('babel-polyfill')
+
+
+
--- packages/js-go-channels/rollup.config.js
+++ packages/js-go-channels/rollup.config.js
@@ -1,37 +1,28 @@
+import fs from 'fs'
+import path from 'path'
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
+import sourcemaps from 'rollup-plugin-sourcemaps'
 import commonjs from 'rollup-plugin-commonjs';
-import fs from 'fs'
 
-// .babelrc doesn't exist because it overrides everything and tests
-// need a different babel config
-const babelrc = JSON.parse(fs.readFileSync('./babelrc.json'))
-const babelConfig = Object.assign(
-  {
-    exclude: 'node_modules/**',
-    runtimeHelpers: true,
-  },
-  babelrc
-)
+const babelrc = JSON.parse(fs.readFileSync(path.join(__dirname, '.babelrc')))
+const babelConfig = babelrc.env['legacy-rollup-umd']
+
+babelConfig.exclude = 'node_modules/**'
+babelConfig.runtimeHelpers = true
 
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/index.js',
-    format: 'cjs'
+    file: 'dist/legacy-umd/index.js',
+    format: 'umd'
   },
-  name: 'redux-go-channels',
+  name: 'js-go-channels',
+  sourcemap: true,
   plugins: [
-    commonjs({
-      include: 'node_modules/**',
-    }),
-    resolve({
-      module: true,             
-      jsnext: true,
-      main: true,
-      browser: true,
-      modulesOnly: true,
-     }),
+    sourcemaps(),
     babel(babelConfig),
+    resolve(),
+    commonjs(),
   ],
 }
